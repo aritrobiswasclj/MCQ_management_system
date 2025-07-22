08e0ef5bdbadc38e115ca2dfb133a10eb49b82d9
Aritro Shankar Biswas <aritrobiswasclj@gmail.com>
2025-07-22 13:34:52 +0600

    good

--- db/Dup_Key_Error_Fix.sql
+++ db/Dup_Key_Error_Fix.sql
@@ -1,10 +1,10 @@
-SELECT setval('users_user_id_seq', (SELECT MAX(user_id) + 1 FROM users), false);
+--SELECT setval('users_user_id_seq', (SELECT MAX(user_id) + 1 FROM users), false);
 --SELECT setval('question_question_id_seq', (SELECT MAX(question_id) + 1 FROM question), false);
 --SELECT setval('category_category_id_seq', (SELECT MAX(category_id) + 1 FROM category), false);
 --SELECT setval('institution_institution_id_seq', (SELECT MAX(institution_id) + 1 FROM institution), false);
 --SELECT setval('tag_tag_id_seq', (SELECT MAX(tag_id) + 1 FROM tag), false);
-<<<<<<< HEAD
-SELECT setval('subject_subject_id_seq', (SELECT MAX(subject_id) + 1 FROM subject), false);
-=======
 
->>>>>>> b00746abeed70aa1b59704f2008f35f6473c512b
+--SELECT setval('subject_subject_id_seq', (SELECT MAX(subject_id) + 1 FROM subject), false);
+
+
+
--- db/search.sql
+++ db/search.sql
@@ -1,6 +1,4 @@
 SELECT c.category_id, c.category_name, s.subject_id, s.subject_name
 FROM category c,subject s
-WHERE c.subject_id = c.subject_id;
+WHERE c.subject_id = s.subject_id 
 
-select *
-from users;
