31c5c07443065d5148ef80d000775afcb49d4bc6
Aritro Shankar Biswas <aritrobiswasclj@gmail.com>
2025-07-30 02:48:37 +0600

    all music data given

--- db/BackgroundMusicFix.sql
+++ db/BackgroundMusicFix.sql
@@ -2,5 +2,12 @@
 ADD COLUMN uploaded_by INTEGER,
 ADD CONSTRAINT fk_background_music_user FOREIGN KEY (uploaded_by) REFERENCES users(user_id); */
 
-ALTER TABLE background_music
-ADD COLUMN is_non_copyright BOOLEAN DEFAULT TRUE;
+/* ALTER TABLE background_music
+ADD COLUMN is_non_copyright BOOLEAN DEFAULT TRUE; */
+
+update background_music
+
+set file_path  = 'db/' || file_path
+
+where file_path not like 'db/%';
+commit;
